46054a3e22b573d306638dbda6542fd562a0c7ea
月と猫 - LunaNeko <lunaticlegacy@163.com>
2025-09-07 17:52:44 +0800

    Added a function for the batch generation of 2-dimensional Perlin Noise in C function that can be called in Python. And also, given a new testing example for batch generation of Perlin Noice.

--- examples/2d_batch_generation.py
+++ examples/2d_batch_generation.py
@@ -2,12 +2,9 @@ import noise
 import numpy as np
 from tqdm import tqdm
 
-def callback(n: float):
-    print(f"Now process: {n * 100:.4f}%")
-
 print("Hello world!")
 p: np.ndarray[np.float32] = noise.batch_pnoise2(
-    0, 0, 1000, 1000, 1024, 1024, 2, 30, callback
+    0.0, 0.0, 1000.0, 1000.0, 1024.0, 1024.0, 2.0, 30.0
 )
 print("Finished.")
 
